648d9d592ca21e5b23f00cdafdd721cb2a43236b
michael-conway <mike.conway@nih.gov>
2022-06-23 16:43:12 -0400

    fix date conversion error

--- ga4gh-console/src/main/java/org/irods/jargon/ga4gh/dos/console/utils/Ga4ghConsoleVersion.java
+++ ga4gh-console/src/main/java/org/irods/jargon/ga4gh/dos/console/utils/Ga4ghConsoleVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.ga4gh.dos.console.utils;
 public final class Ga4ghConsoleVersion {
  public static String VERSION="0.0.2-SNAPSHOT";
- public static String BUILD_TIME="2022-06-23T12:45:04Z";
+ public static String BUILD_TIME="2022-06-23T20:33:25Z";
 }
