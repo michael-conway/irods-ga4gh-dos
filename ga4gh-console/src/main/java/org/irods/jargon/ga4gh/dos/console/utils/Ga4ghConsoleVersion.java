0216c6e21dc4dfa8783262f6b26802187f03d651
michael-conway <mike.conway@nih.gov>
2022-06-23 08:53:57 -0400

    1.2.0 clean compile

--- ga4gh-console/src/main/java/org/irods/jargon/ga4gh/dos/console/utils/Ga4ghConsoleVersion.java
+++ ga4gh-console/src/main/java/org/irods/jargon/ga4gh/dos/console/utils/Ga4ghConsoleVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.ga4gh.dos.console.utils;
 public final class Ga4ghConsoleVersion {
  public static String VERSION="0.0.2-SNAPSHOT";
- public static String BUILD_TIME="2022-06-22T12:53:03Z";
+ public static String BUILD_TIME="2022-06-23T12:45:04Z";
 }
