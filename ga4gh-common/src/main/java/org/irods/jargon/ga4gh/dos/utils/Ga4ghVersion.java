2334651cd0927527c3459d5d74a34e9ead65951e
Mike Conway <Michael.c.conway@gmail.com>
2023-03-22 11:28:25 -0400

    ready to move to master

--- ga4gh-common/src/main/java/org/irods/jargon/ga4gh/dos/utils/Ga4ghVersion.java
+++
@@ -1,5 +0,0 @@
-package org.irods.jargon.ga4gh.dos.utils;
-public final class Ga4ghVersion {
- public static String VERSION="0.0.2-SNAPSHOT";
- public static String BUILD_TIME="2022-07-07T17:51:52Z";
-}
